603445683f56bfdf769a07563d1faa9409194f11
Minkyung Kim <min@Minkyungui-MacBookPro.local>
2020-07-14 13:51:54 +0900

    Banner

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom'
+ 
 const Home = () => {
     return (
-        <div>
-            Hello from homepage
-        </div>
+            <Hero>
+                <Banner title="luxiours rooms" subtitle="delux rooms starting from $299"> <Link to="/rooms" className="btn-primary"> Our rooms </Link></Banner>
+            </Hero>
     )
 }
 
--- src/Pages/Rooms.js
+++ src/Pages/Rooms.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom'
 
 const Rooms = () => {
     return (
         <div>
-            hello from Rooms page
+            <Hero hero='roomsHero'>
+                <Banner title="Our rooms"> <Link to="/" className="btn-primary"> Retunr Home </Link></Banner>
+            </Hero>
         </div>
     )
 }
